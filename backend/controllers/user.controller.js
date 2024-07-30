9cb4962d2e167dc4d167741cbf8f0db5c362f2be
Tushar151997 <tuschaudhary22@gmail.com>
2024-07-30 23:48:13 +0530

    "Added SECRET_KEY to .env file and implemented user registration and login functionality with JWT authentication in user.controller.js"

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, password, role } = req.body;
@@ -26,7 +27,51 @@ export const register = async (req, res) => {
       password: hashedPassword,
       role,
     });
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
-  }
+  } catch (error) {}
+};
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res
+        .status(400)
+        .json({ message: "Something is missing", success: false });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password", success: false });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password", success: false });
+    }
+    // check role is correct or not
+    if (role != user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with current role",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullname}`,
+        success: true,
+      });
+  } catch (error) {}
 };
